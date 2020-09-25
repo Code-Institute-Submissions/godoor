0c2ad141940d6edd919ec89b8e1724ba0eae2c25
geoeskel <geoeskel@gmail.com>
2020-09-25 11:50:15 +0000

    added more game script, changed the draw size to fit the screen better

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-#draw {
-    border: 1px solid black;
-    width: 100%;
+window.addEventListener("load",
+function() {
+    //Draw box setup
+    var draw_width = 800;
+    var draw_height = 600;
+    var gameLive = true;
 
+    //Levevl setup
+    var currentLevel = 1;
+    var currentLife = 5;
+    var colour = "#" + ((1 << 24) * Math.random() | 0).toString(16);
 }
 
+
+
+)
